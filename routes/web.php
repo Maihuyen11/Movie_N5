ef06036a6b6eef8d171d430d4a033beae3984d8c
tby112 <tbyen05@gmail.com>
2026-04-07 08:27:26 +0700

    commit yc1

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+Route::get('/', [App\Http\Controllers\MovieController1::class, 'index']);
+Route::get('/theloai/{id}', [App\Http\Controllers\MovieController1::class, 'theloai']);
+Route::post('/timkiem', [App\Http\Controllers\MovieController1::class, 'timkiem']);
+Route::get('/chitiet/{id}', [App\Http\Controllers\MovieController1::class, 'chitiet'])->name('movie.chitiet');
